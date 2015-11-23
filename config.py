71e95d36c5b0f27cb1161b36b2532de4d0b40c3e
acchan <acchan@openmailbox.org>
2015-11-23 18:53:48 -0200

    Rename URI -> URL to match Heroku\'s recommendation

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '5g\x01\xb4u}\xe2\xe7\xb1\x94\x12\xf1\x11^\\n\x15,\xf9(e|\xd5\x0f'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+    SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevConfig(BaseConfig):
